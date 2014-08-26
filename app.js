f0bb2e33f59df54ab92eac097cf57a647bac9321
ronkeizer <ronkeizer@gmail.com>
2014-08-25 17:37:48 -0700

    add package.json and api key check

--- app.js
+++ app.js
@@ -1,17 +1,31 @@
 var application_root = __dirname,
     express = require( 'express' ), 
     child_process = require('child_process'), 
+    helmet = require('helmet'),
+    _ = require('underscore'),
     fs = require('fs');
 
 var app = express();
+var port = 3001;
 
 app.configure(function() {
     app.use(express.bodyParser() );
     app.use(express.methodOverride() );
     app.use(app.router);
     app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+    app.use(helmet());    // Some security measures
+    app.use(helmet.xframe('deny'));
+    app.use(helmet.xssFilter());
+    app.use(helmet.nosniff());
 });
 
+// implement some kind of apikey check here
+function check_api_key (apikey) { 
+    valid_apikeys = [ apikey ]; 
+    return(_.indexOf(valid_apikeys, apikey) >= 0)
+}
+
+// Run on command line
 function run_cmd (cmdline, res) {
     var child = child_process.exec (cmdline, { cwd: "R" }, function (error, stdout, stderr) {
       if (/^[\],:{}\s]*$/.test(stdout.replace(/\\["\\\/bfnrtu]/g, '@').
@@ -27,20 +41,23 @@ function run_cmd (cmdline, res) {
     });
 };
 
-// routing
+// Routing
 app.post( '/api/:apikey/:script', function(req, res) {
     input = req.body;
-    cmdline = "Rscript "+req.params.script+".R";
-    if (typeof(input) !== 'undefined') {
-      for (var key in input) {
-        cmdline += " "+key+"="+input[key];
-      }
+    if (check_api_key(req.params.apikey)) {
+        // Create commandline
+        cmdline = "Rscript "+req.params.script+".R";
+        if (typeof(input) !== 'undefined') {
+          for (var key in input) {
+            cmdline += " "+key+"="+input[key];
+          }
+        }
+        // and run
+        run_cmd(cmdline, res);        
     }
-    run_cmd(cmdline, res);        
 });
 
-//Start server
-var port = 3001;
+// Start server
 app.listen( port, function() {
     console.log( 'Express server listening on port %d in %s mode', port, app.settings.env );
 });
