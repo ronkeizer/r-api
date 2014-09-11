7b6d7426e2b9357e57cc952df598855168efceb3
Ron Keizer <ronkeizer@rons-mac-mini.ucsf.edu>
2014-09-11 11:48:53 -0700

    fixed server

--- app.js
+++ app.js
@@ -1,27 +1,32 @@
+// Libraries
 var application_root = __dirname,
-    express = require( 'express' ), 
-    child_process = require('child_process'), 
+    express = require( 'express' ), // uses >4.0
+    morgan         = require('morgan'), // logger
+    bodyParser     = require('body-parser'),
+    methodOverride = require('method-override'),
+    child_process = require('child_process'),
     helmet = require('helmet'),
     _ = require('underscore'),
     fs = require('fs');
 
-var app = express();
-var port = 3001;
+// Define the APIs in r_apis.json
+var apis = require('./r_apis.json');
 
-app.configure(function() {
-    app.use(express.bodyParser() );
-    app.use(express.methodOverride() );
-    app.use(app.router);
-    app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-    app.use(helmet());    // Some security measures
-    app.use(helmet.xframe('deny'));
-    app.use(helmet.xssFilter());
-    app.use(helmet.nosniff());
-});
+// Server (Express 4.0)
+var app  = express();
+var port = 3001;
+app.use(express.static(__dirname + '/public')); 	// set the static files location /public/img will be /img for users
+app.use(morgan('dev')); 			        // log every request to the console
+app.use(bodyParser()); 					// pull information from html in POST
+app.use(methodOverride()); 				// simulate DELETE and PUT
+app.use(helmet());                                      // Some security measures
+app.use(helmet.xframe('deny'));
+app.use(helmet.xssFilter());
+app.use(helmet.nosniff());
 
 // implement some kind of apikey check here
-function check_api_key (apikey) { 
-    valid_apikeys = [ apikey ]; 
+function check_api_key (apikey) {
+    valid_apikeys = [ apikey ];
     return(_.indexOf(valid_apikeys, apikey) >= 0)
 }
 
@@ -30,7 +35,7 @@ function run_cmd (cmdline, res) {
     var child = child_process.exec (cmdline, { cwd: "R" }, function (error, stdout, stderr) {
       if (/^[\],:{}\s]*$/.test(stdout.replace(/\\["\\\/bfnrtu]/g, '@').
         replace(/"[^"\\\n\r]*"|true|false|null|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?/g, ']').
-        replace(/(?:^|:|,)(?:\s*\[)+/g, ''))) { 
+        replace(/(?:^|:|,)(?:\s*\[)+/g, ''))) {
           res.send(stdout);
       } else {
           res.send({ error: "Invalid JSON object received from script."});
@@ -41,26 +46,40 @@ function run_cmd (cmdline, res) {
     });
 };
 
-// Routing
-app.post( '/api/:apikey/:script', function(req, res) {
+// Function to call R
+call_R = function(req, res, apis) {
     input = req.body;
     if (check_api_key(req.params.apikey)) {
         // Create commandline
-        cmdline = "Rscript "+req.params.script+".R";
-        if (typeof(input) !== 'undefined') {
-          for (var key in input) {
-            cmdline += " "+key+"="+input[key];
-          }
-        }
-        // and run
-        run_cmd(cmdline, res);        
+	if (_.has(apis, req.params.script)) {
+            var api = apis[req.params.script];
+            var open = apis[req.params.script].file;
+	    if (api.available) {
+		cmdline = "Rscript " + api.file;
+		if (typeof(input) !== 'undefined') {
+		    for (var key in input) {
+			cmdline += " "+key+"="+input[key];
+		    }
+		}
+		// and run
+		run_cmd(cmdline, res);
+	    } else {
+	      res.send({ error: "Error: Requested API not available for use at this moment."});
+	    }
+	} else {
+	  res.send({ error: "Error: Requested API not found."});
+	}
     }
-});
+};
+
+// API accessible from both GET and POST
+app.get( '/api/:apikey/:script', function (req,res) { call_R (req, res, apis) } );
+app.post( '/api/:apikey/:script', function (req,res) { call_R(req, res, apis) } );
 
 // Start server
 app.listen( port, function() {
-    console.log( 'Express server listening on port %d in %s mode', port, app.settings.env );
+    console.log( 'API server listening on port %d in %s mode', port, app.settings.env );
 });
 
-// Example API calls:
+// Example API call:
 // curl -i -X POST -d 'n_samp=100' http://localhost:3001/api/i81234/runif
